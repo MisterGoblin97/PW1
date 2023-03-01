4deef564963da469b235f57f6c2df0dc1a1db720
MisterGoblin96 <krestinenko.matvey@gmail.com>
2023-03-01 11:16:37 +0300

    Добавьте файлы проекта.

---
+++ PW1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PW1
+{
+    class Car
+    {
+        public double maxSpeed;
+        public string name;
+
+        public Car(double maxSpedd,string name)
+        { 
+            this.maxSpeed = maxSpedd;
+            this.name = name;
+        }
+
+        public  void propertyCar(double maxSpeed, string name)
+        {
+            Console.WriteLine("Speed: " + maxSpeed + " Name: " + name);
+        }
+
+
+
+
+        static void Main()
+        {
+            Car CarFirst = new(120, "Vaz2108");
+           
+            Console.WriteLine(CarFirst.propertyCar);
+        }
+    }
+}
